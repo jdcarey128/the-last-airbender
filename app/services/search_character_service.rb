3c0def43576b47d59fc6e86945b82a21526c97b0
Joshua Carey <jdcarey128@gmail.com>
2020-12-11 09:41:36 -0700

    Define service to consume air bender api

---
+++ app/services/search_character_service.rb
@@ -0,0 +1,16 @@
+class SearchCharacterService
+
+  def self.nation_characters(nation)
+    json = Faraday.get('https://last-airbender-api.herokuapp.com/api/v1/characters') do |req|
+      req.params['affiliation'] = format_nation(nation)
+    end
+    JSON.parse(json.body, symbolize_names: true)
+  end
+
+
+  private 
+    def self.format_nation(nation)
+      nation.sub('_', ' ')
+    end
+  
+end
---
+++ spec/services/search_character_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe SearchCharacterService do 
+  describe 'nation_characters' do 
+    it 'returns a parsed character array' do 
+      result = SearchCharacterService.nation_characters('fire_nation')
+  
+      expect(result).to be_a(Array)
+  
+      expect(result.first).to have_key(:allies)
+      expect(result.first[:allies]).to be_a(Array)
+  
+      expect(result.first).to have_key(:enemies)
+      expect(result.first[:enemies]).to be_a(Array)
+  
+      expect(result.first).to have_key(:name)
+      expect(result.first[:name]).to be_a(String)
+  
+      expect(result.first).to have_key(:affiliation)
+      expect(result.first[:affiliation]).to be_a(String)
+    end
+  end
+end
